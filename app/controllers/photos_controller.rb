5b1ea1775c4cada1a3a3b8d2fb264cac53ff11ec
emacip <emacipbelmonte@gmail.com>
2017-11-20 11:23:26 +0000

    Test Photo, List photos from a photographer, Create a photo with valid attributes

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,10 +6,20 @@ class PhotosController < ApplicationController
     json_response(@photographer.photos)
   end
 
+  # POST /photographers/:photographer_id/photos
+  def create
+    @photographer.photos.create!(photo_params)
+    json_response(@photographer, :created)
+  end
+
   private
+
+  def photo_params
+    params.permit(:url, :code)
+  end
 
   def set_photographer
     @photographer = Photographer.find(params[:photographer_id])
   end
-
+  
 end
--- spec/requests/photographers_spec.rb
+++ spec/requests/photographers_spec.rb
@@ -4,51 +4,34 @@ require 'rails_helper'
 
 RSpec.describe 'Photographers API', type: :request do
   # initialize test data
-  let!(:photographer) { create(:photographer) }
-  let!(:photos) { create_list(:photo, 20, photographer_id: photographer.id) }
-  let(:photographer_id) { photographer.id }
+  let!(:photographer) { create_list(:photographer, 20) }
 
   # Test suite for GET /photographers
   describe 'GET /photographers' do
     # make HTTP get request before each example
     before { get '/photographers' }
 
-    it 'returns photographers' do
-      expect(json).not_to be_empty
-      expect(json.size).to eq(1)
+    context 'when photographers exists' do
+      it 'returns photographers' do
+        expect(json).not_to be_empty
+        expect(json.size).to eq(20)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
     end
 
-    it 'returns status code 200' do
-      expect(response).to have_http_status(200)
-    end
-  end
+    context 'when photographers does not exists' do
+      let(:photographer) { 0 }
 
-  # Test suite for GET /photographers/:photographer_id/photos
-  describe 'GET /photographers/:photographer_id/photos' do
-    # make HTTP get request before each example
-    before { get "/photographers/#{photographer_id}/photos" }
-
-    context 'when photographer exists' do
       it 'returns status code 200' do
         expect(response).to have_http_status(200)
       end
 
-      it 'returns all photographer photos' do
-        expect(json.size).to eq(20)
-      end
-    end
-
-    context 'when photographer does not exist' do
-      let(:photographer_id) { 0 }
-
-      it 'returns status code 404' do
-        expect(response).to have_http_status(404)
-      end
-
-      it 'returns a not found message' do
-        expect(response.body).to match(/Couldn't find Photographer/)
+      it 'returns a empty Array' do
+        expect([]).to be_empty
       end
     end
   end
-
 end
